c27fafa009928be279e4df52d180fd1b301cb075
helsaalika <helsa.alika.tif421@polban.ac.id>
2022-02-13 20:44:45 +0700

    revisi : penambahan komentar pada Soal 5

--- ConvertDataType/src/operators/ConvertDataType.java
+++ ConvertDataType/src/operators/ConvertDataType.java
@@ -14,3 +14,19 @@ public class ConvertDataType {
 		System.out.println(b);
 	}
 }
+
+/* 
+ * Komentar Program
+ * Program untuk mengkonversi dari double ke float ke byte lalu dari long ke int lalu ke short
+ * 
+ * Pertanyaan 
+ * 1. Jelaskan output nilai dari variabel b 
+ * 		Output yang dihasilkan oleh variabel b adalah 10 dengan tipe data byte.
+ * 
+ * 2. Jelaskan apa yang berubah dari variabel d menjadi variabel b setelah dilakukan cast ?
+ * 		variabel d dengan tipe data double dikonversikan ke tipe data float pada variabel f
+ * 		lalu dikonversikan kembali ke tipe data byte setelah memanggil modul methodOne
+ * 		di modul methodOne terjadi pengkonversian dari long ke int lalu ke short. Variabel b
+ * 		sekarang bertipe byte.
+ */
+
