a1681da30728f203f76662bab065867a944a3ee5
helsaalika <helsa.alika.tif421@polban.ac.id>
2022-02-13 21:03:03 +0700

    revisi : penambahan komentar pada Soal 3

--- Variables/src/variables/Constants2.java
+++ Variables/src/variables/Constants2.java
@@ -9,3 +9,20 @@ public class Constants2 {
 	}
 
 }
+
+/*
+ * Komentar Program
+ * penggunaan final double dan public static final double pada Class Contants2
+ * 
+ *  Pertanyaan :
+ *  1. Bagaimana output dari masing – masing class Constants dan Constants2 ?
+ *  	Output yang dihasilkan dari masing-masing class adalah sama, 
+ *  	yaitu Paper size in centimeters : 21.59 by 27.94
+ *  
+ *  2. Apa perbedaan penggunaan final double dengan public static final double ?
+ *  	Final double membuat nilainya tidak dapat diubah atau sama saja dengan konstanta 
+ *  	dan hanya digunakan pada modul final double itu berada, disini berarti pada modul main
+ *  	Public static final double sama hal nya dengan final double yakni nilainya tidak 
+ *  	dapat diubah, hanya aja variabel ini bisa diakses di modul manapun jika dipanggil 
+ *  	karena bersifat public. 
+ */
